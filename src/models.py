f9d3c7ae3415db7e63d2672d30b932def8749eac
Yarumis Rodriguez <107522978+YarumisGinanRM@users.noreply.github.com>
2023-07-11 18:10:02 +0000

    People y Planets agregadas a data models

--- src/models.py
+++ src/models.py
@@ -1,12 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    creation_date = db.Column(db.DateTime, nullable=False)
+    people = db.relationship('People', backref="User", lazy=True)
+    planets = db.relationship('Planets', backref="User", lazy=True)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +23,27 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    __tablename__ = 'People'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    gender = db.Column(db.String(120), nullable=False)
+    birth_year = db.Column(db.String(120), nullable=False)
+    homeworld = db.Column(db.String(120), nullable=False)
+    creation_date = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False)
+
+class Planets(db.Model):
+    __tablename__ = 'Planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    climate = db.Column(db.String(120), nullable=False)
+    terrain = db.Column(db.String(120), nullable=False)
+    population = db.Column(db.String(120), nullable=False)
+    gravity = db.Column(db.String(120), nullable=False)
+    diameter = db.Column(db.String(120), nullable=False)
+    creation_date = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False)
