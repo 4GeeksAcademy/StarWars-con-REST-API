8c41ac54f49baf10292fa9be92da473b75b93b76
Yarumis Rodriguez <107522978+YarumisGinanRM@users.noreply.github.com>
2023-07-12 00:47:40 +0000

    funciones contructoras insertadas

--- src/admin.py
+++ src/admin.py
@@ -13,5 +13,6 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(People, db.session))
     admin.add_view(ModelView(Planets, db.session))
+    # admin.add_view(ModelView(Favoritos, db.session))
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -35,6 +36,30 @@ class People(db.Model):
     creation_date = db.Column(db.DateTime, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False)
 
+    def __init__(self, name, description, gender, birth_year, homeworld, user_id):
+        self.name = name
+        self.description = description
+        self.gender = gender
+        self.birth_year = birth_year
+        self.homeworld = homeworld 
+        self.user_id = user_id
+        self.creation_date = date.today()
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "gender": self.gender,
+            "birth_year": self.birth_year,
+            "homeworld": self.homeworld,
+            "user_id": self.user_id,
+            "creation_date": self.creation_date,
+        }
+
 class Planets(db.Model):
     __tablename__ = 'Planets'
     id = db.Column(db.Integer, primary_key=True)
@@ -47,3 +72,38 @@ class Planets(db.Model):
     diameter = db.Column(db.String(120), nullable=False)
     creation_date = db.Column(db.DateTime, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False)
+
+    def __init__(self, name, description, climate, terrain, population, gravity, diameter, user_id):
+        self.name = name
+        self.description = description
+        self.climate = climate
+        self.terrain = terrain
+        self.population = population
+        self.gravity = gravity
+        self.diameter = diameter
+        self.user_id = user_id
+        self.creation_date = date.today()
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "population": self.population,
+            "gravity": self.gravity,
+            "diameter": self.diameter,
+            "user_id": self.user_id,
+            "creation_date": self.creation_date,
+        }
+
+# class Favoritos(db.Model):
+#     __tablename__ = 'Favoritos'
+#     id = db.Column(db.Integer, primary_key=True)
+#     people_id = db.Column(db.Integer, db.ForeignKey("People.id"), nullable=False)
+#     planets_id = db.Column(db.Integer, db.ForeignKey("Planets.id"), nullable=False)
+#     creation_date = db.Column(db.DateTime, nullable=False)
